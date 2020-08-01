357007d327bfe991c91280c7f4c0eefc91b943a8
Jeremiah Black <jeremiahblackol@gmail.com>
2020-08-01 16:59:10 -0600

    added minor styling

--- faded_philosophy/faded_philosophy/src/Header.js
+++ faded_philosophy/faded_philosophy/src/Header.js
@@ -9,36 +9,39 @@ const Header = () => {
     return (
       <BrowserRouter>
           <header>
-            <h1>FADED</h1>
-            <h2>PHILOSOPHY</h2>
+            <section>
+              <h1>FADED</h1>
+              <h2>PHILOSOPHY</h2>
+            </section>
             <nav>
               <NavLink to={"/"}>
-                <button className="submitButton">
+                <button className="navButton">
                     HOME
                 </button>
               </NavLink> 
-              <Route exact path="/" render={() => <Home />} />
+              {/* <Route exact path="/" render={() => <Home />} /> */}
               <NavLink to={"/strains"}>
-                <button className="submitButton">
+                <button className="navButton">
                     STRAINS
                 </button>
               </NavLink> 
-              <Route exact path="/strains" render={() => <Strains />} />
               <NavLink to={"/spirits"}>
-                <button className="submitButton">
+                <button className="navButton">
                     SPIRITS
                 </button>
-                </NavLink> 
+              </NavLink> 
+              <NavLink to={"/philosophers"}>
+                <button className="navButton">
+                    <span>PHILOSOPHERS</span>
+                </button>
+              </NavLink> 
+              {/* <Route exact path="/strains" render={() => <Strains />} /> */}
               {/*
+                <Route exact path="/spirits" render={() => <Spirits />} />
+              
 
               // always want it to render the main component, pass props in to tell it what to render
               //inside the main
-              <Route exact path="/spirits" render={() => <Spirits />} />
-              <NavLink to={"/philosophers"}>
-                <button className="submitButton">
-                    PHILOSOPHERS
-                </button>
-              </NavLink> 
               <Route exact path="/philosophers" render={() => <Philosophers />} /> 
               */}
             </nav>
