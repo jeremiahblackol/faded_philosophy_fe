c52b6cc05c78f9f15c2b35aea8ce41b7dcd5ef91
Jeremiah Black <jeremiahblackol@gmail.com>
2020-08-02 22:29:57 -0600

    added a data file

---
+++ faded_philosophy/faded_philosophy/src/data.js
@@ -0,0 +1,74 @@
+export const strains = [
+
+]
+
+export const combinations = {
+    anger: {
+        philosopher: {
+            style: ,
+            quote: ,
+            img_url:
+        }
+        strain: {
+            name: ,
+            type: ,
+            img_url:
+        },
+        spirit: [],
+    },
+    disgust: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    fear: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    joy: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    sadness: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    openness: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    conscientiousness: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    extroversion: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    agreeableness: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    analytical: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    confident: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    },
+    tentative: {
+        philosopher: ,
+        strain: ,
+        spirit: ,
+    }
+}
