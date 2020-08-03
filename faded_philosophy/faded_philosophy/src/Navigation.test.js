06801f0da05501e79765cd2bd187fba8dc91e5cf
Jeremiah Black <jeremiahblackol@gmail.com>
2020-08-02 19:18:58 -0600

    reinstalled dependencies finished navigation tests

--- faded_philosophy/faded_philosophy/src/Navigation.test.js
+++ faded_philosophy/faded_philosophy/src/Navigation.test.js
@@ -1,22 +1,43 @@
 import React from "react";
-import Navigation from "./Header";
+import Navigation from "./Navigation";
 import { render } from "@testing-library/react";
 import {
-    BrowserRouter,
-    Switch,
-    Route,
-    Link
+    BrowserRouter
   } from "react-router-dom";
 
 
 
 describe("Navigation", () => {
 
-  it("Should render navigation", () => {
+  it("Should render philosophers link", () => {
     const { getByText } = render(<BrowserRouter><Navigation /></BrowserRouter>);
 
-    const philosophersButton = getByText('Philosophers');
+    const philosophersButton = getByText('PHILOSOPHERS');
 
     expect(philosophersButton).toBeInTheDocument();
+  });
+
+  it("Should render strains link", () => {
+    const { getByText } = render(<BrowserRouter><Navigation /></BrowserRouter>);
+
+    const strainsButton = getByText('STRAINS');
+
+    expect(strainsButton).toBeInTheDocument();
+  });
+
+  it("Should render spirits link", () => {
+    const { getByText } = render(<BrowserRouter><Navigation /></BrowserRouter>);
+
+    const spiritsButton = getByText('SPIRITS');
+
+    expect(spiritsButton).toBeInTheDocument();
+  });
+
+  it("Should render home link", () => {
+    const { getByText } = render(<BrowserRouter><Navigation /></BrowserRouter>);
+
+    const homeButton = getByText('HOME');
+
+    expect(homeButton).toBeInTheDocument();
   });
 });
